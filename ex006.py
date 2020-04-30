d1d09351fd63282cae63d816fe3565842d160ccf
Naissinger <64567310+Naissinger@users.noreply.github.com>
2020-04-30 00:30:18 -0300

    Operadores Aritméticos.

---
+++ ex006.py
@@ -0,0 +1,5 @@
+v = int(input('Digite um número: '))
+
+print('\nO Dobro de {} vale {}.'.format(v, v*2))
+print('O Triplo de {} vale {}.'.format(v, v*3))
+print('A raiz quadrada de {} é igual a {:.0f}.'.format(v, v**(1/2)))
