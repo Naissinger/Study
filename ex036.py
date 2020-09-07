293e9813920a6c9ddeebced54b6cda208398d6e3
Naissinger <64567310+Naissinger@users.noreply.github.com>
2020-09-07 18:47:14 -0300

    Add files via upload
    Empréstimo Negado.

---
+++ ex036.py
@@ -0,0 +1,12 @@
+casa = float(input('Qual o valor da casa? R$'))
+salario = float(input('Qual o seu salário? R$'))
+anos = int(input('Em quantos anos você pretende pagar? '))
+pmensal = casa / (anos*12)
+limite = salario * 0.3
+
+print('\nPara pagar uma casa de R${} em {} anos a prestação será de R${:.2f}'.format(casa, anos, pmensal))
+
+if pmensal <= limite:
+    print('\nParabéns o seu empréstimo foi CONCEDIDO !')
+else:
+    print('\nDesculpe, o seu empréstimo foi NEGADO !')
